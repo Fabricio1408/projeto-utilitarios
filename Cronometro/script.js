76578960d08094c258300494d8c853ce86880fb2
Fabricio Da Silva Ribeiro <102611358+Fabricio1408@users.noreply.github.com>
2024-02-14 10:59:16 -0300

    Update script.js
    add relogio

--- Cronometro/script.js
+++ Cronometro/script.js
@@ -1,3 +1,29 @@
+const horas = document.getElementById('horas');
+const minutos = document.getElementById('minutos');
+const segundos = document.getElementById('segundos');
+
+const relogio = setInterval(function time() {
+    let dateToday = new Date();
+    let hr = dateToday.getHours();
+    let min = dateToday.getMinutes();
+    let s = dateToday.getSeconds();
+
+    if (hr < 10) hr = '0' + hr;
+
+    if (min < 10) min = '0' + min;
+
+    if (s < 10) s = '0' + s;
+
+    horas.textContent = hr;
+    minutos.textContent = min;
+    segundos.textContent = s;
+
+})
+
+
+
+
+
 const minutesEl = document.querySelector("#minutes");
 const secondsEl = document.querySelector("#seconds");
 const millisecondsEl = document.querySelector("#milliseconds");
@@ -17,7 +43,12 @@ pausebtn.addEventListener("click", pauseTimer)
 resumebtn.addEventListener("click", resumeTimer)
 resetbtn.addEventListener("click", resetTimer)
 
+
+
+
 function startTimer(){
+    
+
     interval = setInterval(()=> {
             if(!ispaused){
                 milliseconds += 10;
@@ -32,6 +63,7 @@ function startTimer(){
                 minutesEl.textContent = formatTime(minutes);
                 secondsEl.textContent = formatTime(seconds);
                 millisecondsEl.textContent = formatTimeM(milliseconds);
+
             }
         }, 10);
         startbtn.style.display = 'none'
@@ -56,7 +88,8 @@ function resetTimer(){
     seconds = 0;
     milliseconds = 0;
     ispaused = false
-    
+
+
     minutesEl.textContent = '00'
     secondsEl.textContent = '00'
     millisecondsEl.textContent = '000'
@@ -64,14 +97,16 @@ function resetTimer(){
     startbtn.style.display = 'block'
     pausebtn.style.display = 'none'
     resumebtn.style.display = 'none'
+
+
 }
+
+
 
 function formatTime(time){
     return time < 10 ? `0${time}` : time;
-} //formatando para mais um zero
-
-
+}
 function formatTimeM(time){
     return time < 100 ? `${time}`.padStart(3, "0") : time;
-} //formatando para mais um zero
+}
 
